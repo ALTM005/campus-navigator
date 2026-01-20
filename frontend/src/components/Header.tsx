fd121f7bd302ca9802cf430d3e00e5ba72370865
Aritha Munasinghe <arithaltm@gmail.com>
2026-01-19 19:58:54 -0800

    Minor code changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,6 @@ export default function Header({ health }: Props) {
   return (
     <header className="appbar">
       <h1>Campus Navigator</h1>
-      <span className="pill">{health}</span>
     </header>
   );
 }
